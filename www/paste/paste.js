aac1ce97add7347d7b54399dce0000f6c20dd75c
Robert Williams <robert@increscent.org>
2015-02-02 17:57:34 -0700

    hope this works

--- www/paste/paste.js
+++ www/paste/paste.js
@@ -1,9 +1,5 @@
-var paste_data = document.getElementById('paste-data');
-
 function remove_html_tags(data) {
 	data = data.replace(new RegExp('<', 'g'), '&lt;');
 	data = data.replace(new RegExp('>', 'g'), '&gt;');
 	return data;
 }
-
-paste_data.innerHTML = remove_html_tags(paste_data.innerHTML);
