b7f542455e8e494386c0afd47d0d4b6a24a83713
robert <robert@increscent.org>
2015-04-30 15:46:09 -0600

    fixed frontend naming error

--- www/copy/copy.js
+++ www/copy/copy.js
@@ -1,77 +1,78 @@
 var routes = {
-	text: {
-		controller: textController,
-		template: '/copy/text/text.html'
-	},
-	file: {
-		controller: fileController,
-		template: '/copy/file/file.html'
-	}
+  text: {
+    controller: textController,
+    template: '/copy/text/text.html'
+  },
+  file: {
+    controller: fileController,
+    template: '/copy/file/file.html'
+  }
 };
-var starting_route = 'text';
+
+var STARTING_ROUTE = 'text';
 var templates = {};
 var view_container = document.getElementById('view');
 var websocket_connections = [];
 
 // load all templates
 for (var route in routes) {
-	load_template(route, function (template) {});
+  load_template(route, function (template) {});
 }
 
 function load_template(route, callback) {
-	var template = routes[route].template;
-	if (templates[template]) return callback(templates[template]);
-	ajax_request('GET', template, undefined, function (data) {
-		templates[template] = data;
-		// start with the first route
-		if (route === starting_route) open_route(starting_route);
-		return callback(templates[template]);
-	});
+  var template = routes[route].template;
+  if (templates[template]) return callback(templates[template]);
+  ajax_request('GET', template, undefined, function (data) {
+    templates[template] = data;
+    // start with the first route
+    if (route === STARTING_ROUTE) open_route(STARTING_ROUTE);
+    return callback(templates[template]);
+  });
 }
 
 function close_websockets() {
-	for (var key in websocket_connections) {
-		websocket_connections[key].close();
-		delete websocket_connections[key];
-	}
+  for (var key in websocket_connections) {
+    websocket_connections[key].close();
+    delete websocket_connections[key];
+  }
 }
 
 function open_route(route) {
-	close_websockets();
-	load_template(route, function (template_html) {
-		view_container.innerHTML = template_html;
-		routes[route].controller();
-	});
+  close_websockets();
+  load_template(route, function (template_html) {
+    view_container.innerHTML = template_html;
+    routes[route].controller();
+  });
 }
 
 
 /** global functions **/
 
 function ajax_request(type, url, data, callback) {
-	var request = new XMLHttpRequest();
-	
-	request.open(type.toUpperCase(), url, true);
-	request.setRequestHeader("Content-type","application/json");
-	request.onload = function () {
-		return callback(request.response);
-	};
-	if (type.toLowerCase() === 'post') data = JSON.stringify(data);
-	request.send(data);
+  var request = new XMLHttpRequest();
+
+  request.open(type.toUpperCase(), url, true);
+  request.setRequestHeader("Content-type","application/json");
+  request.onload = function () {
+    return callback(request.response);
+  };
+  if (type.toLowerCase() === 'post') data = JSON.stringify(data);
+  request.send(data);
 }
 
 function update_feedback(feedback_span, feedback, positive) {
-	feedback_span.innerHTML = feedback;
-	if (positive)
-		feedback_span.className = 'feedback feedback-positive';
-	else
-		feedback_span.className = 'feedback feedback-negative';
+  feedback_span.innerHTML = feedback;
+  if (positive)
+    feedback_span.className = 'feedback feedback-positive';
+  else
+    feedback_span.className = 'feedback feedback-negative';
 }
 
 function validate_name(name) {
-	var original = name;
-	var regex = new RegExp(' ', 'g');
-	name = name.replace(regex, '');
-	name = name.toLowerCase();
-	name = decodeURIComponent(encodeURIComponent(name));
-	return name;
+  var original = name;
+  var regex = new RegExp(' ', 'g');
+  name = name.replace(regex, '');
+  name = name.toLowerCase();
+  name = decodeURIComponent(encodeURIComponent(name));
+  return name;
 }
--- www/copy/text/text.js
+++ www/copy/text/text.js
@@ -1,24 +1,24 @@
 function textController() {
-	title_input = document.getElementById('copy-title');
-	data_textarea = document.getElementById('copy-text');
-	feedback_span = document.getElementById('feedback');
-	
-	data_textarea.focus();
-	
-	stream = new Stream();
-	websocket_connections.push(stream);
-	stream.on_error = function (message) {
-		update_feedback(feedback_span, message.error_text, false);
-	};
-	stream.on_success = function (message) {
-		update_feedback(feedback_span, message.success_text, true);
-	};
-	
-	title_input.addEventListener('input', input_handler);
-	data_textarea.addEventListener('input', input_handler);
-	data_textarea.addEventListener('input', resize_textarea);
-	
-	user_input_for_title = false;
+  title_input = document.getElementById('copy-title');
+  data_textarea = document.getElementById('copy-text');
+  feedback_span = document.getElementById('feedback');
+
+  data_textarea.focus();
+
+  stream = new Stream();
+  websocket_connections.push(stream);
+  stream.on_error = function (message) {
+    update_feedback(feedback_span, message.error_text, false);
+  };
+  stream.on_success = function (message) {
+    update_feedback(feedback_span, message.success_text, true);
+  };
+
+  title_input.addEventListener('input', input_handler);
+  data_textarea.addEventListener('input', input_handler);
+  data_textarea.addEventListener('input', resize_textarea);
+
+  user_input_for_title = false;
 }
 
 var title_input;
@@ -29,47 +29,44 @@ var input_change_timeout;
 var user_input_for_title;
 
 function input_handler(e) {
-	update_feedback('', false);
-	// if the user has not changed title input, update it automatically
-	if (!user_input_for_title) {
-		var target = e.target | e.srcElement;
-		console.log(target);
-		if (target.id === 'copy-title') {
-			user_input_for_title = true;
-		} else {
-			update_title();
-		}
-	}
-	// save after one second of not typing
-	clearTimeout(input_change_timeout);
-	input_change_timeout = setTimeout(function () {
-		update_text_stream();
-	}, 1000);
+  update_feedback('', false);
+  // if the user has not changed title input, update it automatically
+  if (!user_input_for_title) {
+    var target = (e.target)? e.target : e.srcElement;
+    if (target.id === 'copy-title') {
+      user_input_for_title = true;
+    } else {
+      update_title();
+    }
+  }
+  // save after one second of not typing
+  clearTimeout(input_change_timeout);
+  input_change_timeout = setTimeout(function () {
+      update_text_stream();
+  }, 1000);
 }
 
 function update_title() {
-	var data = data_textarea.value;
-	// find first space after 10 characters
-	var first_space = data.indexOf(' ', 10);
-	if (first_space < 0) first_space = data.length;
-	first_space = (first_space > 15)? 15 : first_space;
-	title_input.value = data.substring(0, first_space);
+  var data = data_textarea.value;
+  // find first space after 10 characters
+  var first_space = data.indexOf(' ', 10);
+  if (first_space < 0) first_space = data.length;
+  first_space = (first_space > 15)? 15 : first_space;
+  title_input.value = data.substring(0, first_space);
 }
 
 function update_text_stream() {
-	var name = validate_name(title_input.value);
-	var title = title_input.value;
-	var data = data_textarea.value;
-	
-	if (name) stream.send_update(name, title, data);
+  var name = validate_name(title_input.value);
+  var title = title_input.value;
+  var data = data_textarea.value;
+
+  if (name) stream.send_update(name, title, data);
 }
 
 var resize_textarea = function (e) {
-	var target = e.target | e.srcElement;
-	var text = target.value;
-	console.log(target);
-	console.log(text);
-	var lines = (text.match(/\n/g) || []).length;
-	lines++;
-	if (lines > 20) e.srcElement.rows = lines;
+  var target = (e.target) ? e.target : e.srcElement;
+  var text = target.value;
+  var lines = (text.match(/\n/g) || []).length;
+  lines++;
+  if (lines > 20) e.srcElement.rows = lines;
 };
